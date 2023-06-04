d7e18b05f9068fb69b6b146f21458404018b0bcc
freedom00y <32114761+freedom00y@users.noreply.github.com>
2023-06-04 22:47:19 +0800

    contact for real data analysis

--- Simulation_ABS/simu_output_outlier/SummaryOutlier.R
+++ Simulation_ABS/simu_output_outlier/SummaryOutlier.R
@@ -9,12 +9,12 @@ SumTable <- function(filename,n=550)
   return(res)
 }
 
-files=c("n_0.04_550_0.01","n_0.25_550_0.01",
+files=c(#"n_0.04_550_0.01","n_0.25_550_0.01",
         #"n_0.04_550_0.03","n_0.25_550_0.03",
         "n_0.04_550_0.05","n_0.25_550_0.05")
 
 AllRes = data.frame()
-for(k in 1:4)
+for(k in 1:2)
 {
   filename = files[k]
   OneRes = SumTable(filename)
@@ -22,11 +22,11 @@ for(k in 1:4)
 }
 write.csv(AllRes,file="PointOutlier.csv")
 
-files = paste0("curveoutlier_",c("0.04_550_0.01","0.25_550_0.01",
+files = paste0("curveoutlier_",c(#"0.04_550_0.01","0.25_550_0.01",
                                 #"0.04_550_0.03","0.25_550_0.03",
                                  "0.04_550_0.05","0.25_550_0.05"))
 AllRes = data.frame()
-for(k in 1:4)
+for(k in 1:2)
 {
   filename = files[k]
   OneRes = SumTable(filename,n=550)
